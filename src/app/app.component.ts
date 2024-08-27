b34cb26bcae107104591b5a4519e8dc643ca5507
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-27 17:31:51 +0200

    Added dialogs for registration/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+// app.component.ts
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) {}
+
+  // Function to open the user registration dialog
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px',
+    });
+  }
+
+  // Function to open the user login dialog
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px',
+    });
+  }
 }
