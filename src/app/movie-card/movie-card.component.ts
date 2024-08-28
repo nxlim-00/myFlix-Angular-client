a18c159316cd4e8b3a9c0787208e9e3bf6ed21ed
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-28 16:21:59 +0200

    Created movie-card component

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { GetAllMoviesService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrl: './movie-card.component.scss',
+})
+export class MovieCardComponent {
+  movies: any[] = [];
+  constructor(public fetchMovies: GetAllMoviesService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.fetchMovies.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      console.log(this.movies);
+      return this.movies;
+    });
+  }
+}
