72542e81a5dd5d124dd6823b324f5ccb43a72296
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-30 00:16:08 +0200

    Added synopsis dialog function
    Imported SynopsisComponent

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { GetAllMoviesService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SynopsisComponent } from '../synopsis/synopsis.component';
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
-  styleUrl: './movie-card.component.scss',
+  styleUrls: ['./movie-card.component.scss'],
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchMovies: GetAllMoviesService) {}
+
+  constructor(
+    public fetchMovies: GetAllMoviesService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.getMovies();
@@ -19,6 +25,14 @@ export class MovieCardComponent {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  // Function to open the synopsis dialog
+  openSynopsisDialog(movie: any): void {
+    this.dialog.open(SynopsisComponent, {
+      data: movie, // Pass the entire movie object to the dialog
+      width: '500px',
     });
   }
 }
