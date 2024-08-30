e5bda7a9c730ff2cfb0e0b3b7189095f91458a60
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-30 18:09:29 +0200

    Updated Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FavoriteListComponent } from './favorite-list/favorite-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'favorites', component: FavoriteListComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }, // Redirect to welcome by default
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
