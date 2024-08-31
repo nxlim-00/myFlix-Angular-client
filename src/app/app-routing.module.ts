34efb3897d744456f63ae60ff7190d2f1146a42d
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-31 14:25:26 +0200

    Added route to single movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FavoriteListComponent } from './favorite-list/favorite-list.component';
+import { SingleMovieComponent } from './single-movie/single-movie.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
   { path: 'profile', component: UserProfileComponent },
   { path: 'favorites', component: FavoriteListComponent },
+  { path: 'movies/:id', component: SingleMovieComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' }, // Redirect to welcome by default
 ];
 
