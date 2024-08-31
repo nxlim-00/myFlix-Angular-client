3ac8f87b5db0209d5468a0a0174693ae471a3348
nxlim-00 <167754656+nxlim-00@users.noreply.github.com>
2024-08-31 14:22:48 +0200

    Added FavoriteMovies array

--- src/app/user-profile/user-profile.model.ts
+++ src/app/user-profile/user-profile.model.ts
@@ -1,5 +1,6 @@
 export interface UserProfile {
   Username: string;
   Email: string;
-  Birthday: string; // Assuming birthday is stored as a string
+  Birthday: string;
+  FavoriteMovies: string[];
 }
